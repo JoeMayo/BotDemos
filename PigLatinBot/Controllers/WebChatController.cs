b90ef3d37e627e825949d91fe087c14aa949475c
Joe Mayo <jmayo@mayosoftware.com>
2016-04-07 23:16:24 -0700

    Added WebChat control to PigLatinBot's default.htm.

---
+++ PigLatinBot/Controllers/WebChatController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using PigLatinBot.Models;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PigLatinBot
+{
+    public class WebChatController : ApiController
+    {
+        public async Task<string> Get()
+        {
+            string webChatSecret = ConfigurationManager.AppSettings["WebChatSecret"];
+
+            // This technique is insecure because you're exposing your secret in the Web page
+            //return $"<iframe width='400px' height='400px' src='https://webchat.botframework.com/embed/PigLatinBotJoeMayo?s={webChatSecret}'></iframe>";
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://webchat.botframework.com/api/conversations");
+            request.Headers.Add("Authorization", "BOTCONNECTOR " + webChatSecret);
+
+            HttpResponseMessage response = await new HttpClient().SendAsync(request);
+            string responseJson = await response.Content.ReadAsStringAsync();
+            WebChatTokenResponse webChatResponse = JsonConvert.DeserializeObject<WebChatTokenResponse>(responseJson);
+
+            return $"<iframe width='400px' height='400px' src='https://webchat.botframework.com/embed/PigLatinBotJoeMayo?t={webChatResponse.Token}'></iframe>";
+        }
+    }
+}
---
+++ PigLatinBot/Models/WebChatTokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace PigLatinBot.Models
+{
+    public class WebChatTokenResponse
+    {
+        public string ConversationID { get; set; }
+
+        public string Token { get; set; }
+    }
+}
