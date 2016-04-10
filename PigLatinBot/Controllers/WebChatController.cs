6dcb15dabb0e7157167d568bbaeac280ef23b347
Joe Mayo <jmayo@mayosoftware.com>
2016-04-09 19:36:25 -0700

    Added Web Chat control to Pig Latin Bot.

--- PigLatinBot/Controllers/WebChatController.cs
+++ PigLatinBot/Controllers/WebChatController.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using PigLatinBot.Models;
-using System;
+﻿using System;
 using System.Configuration;
 using System.Linq;
 using System.Net.Http;
@@ -16,16 +14,16 @@ namespace PigLatinBot
             string webChatSecret = ConfigurationManager.AppSettings["WebChatSecret"];
 
             // This technique is insecure because you're exposing your secret in the Web page
-            //return $"<iframe width='400px' height='400px' src='https://webchat.botframework.com/embed/PigLatinBotJoeMayo?s={webChatSecret}'></iframe>";
+            return $"<iframe width='400px' height='400px' src='https://webchat.botframework.com/embed/PigLatinBotJoeMayo?s={webChatSecret}'></iframe>";
 
-            var request = new HttpRequestMessage(HttpMethod.Post, "https://webchat.botframework.com/api/conversations");
-            request.Headers.Add("Authorization", "BOTCONNECTOR " + webChatSecret);
+            //var request = new HttpRequestMessage(HttpMethod.Get, "https://webchat.botframework.com/api/tokens");
+            //request.Headers.Add("Authorization", "BOTCONNECTOR " + webChatSecret);
 
-            HttpResponseMessage response = await new HttpClient().SendAsync(request);
-            string responseJson = await response.Content.ReadAsStringAsync();
-            WebChatTokenResponse webChatResponse = JsonConvert.DeserializeObject<WebChatTokenResponse>(responseJson);
+            //HttpResponseMessage response = await new HttpClient().SendAsync(request);
+            //string token = await response.Content.ReadAsStringAsync();
+            //token = token.Replace("\"", "");
 
-            return $"<iframe width='400px' height='400px' src='https://webchat.botframework.com/embed/PigLatinBotJoeMayo?t={webChatResponse.Token}'></iframe>";
+            //return $"<iframe width='400px' height='400px' src='https://webchat.botframework.com/embed/PigLatinBotJoeMayo?t={token}'></iframe>";
         }
     }
 }
