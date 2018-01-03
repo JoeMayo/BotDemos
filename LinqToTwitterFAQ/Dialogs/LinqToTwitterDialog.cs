30803e907aee323ed6d3ca3415110df4de3ba64d
Joe Mayo <jmayo@mayosoftware.com>
2018-01-02 19:47:07 -0800

    Added a QnA Maker demo - LINQ to Twitter FAQ.

---
+++ LinqToTwitterFAQ/Dialogs/LinqToTwitterDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+
+namespace LinqToTwitterFAQ.Dialogs
+{
+    [Serializable]
+    [QnAMaker(
+        subscriptionKey: "<Your Subscription Key Goes Here>",
+        knowledgebaseId: "<Your Knowledge Base ID Goes Here>")]
+    public class LinqToTwitterDialog : QnAMakerDialog
+    {
+    }
+}
